c915b3750d94d91f1d4f06a45d13b354c586bbae
Kamil Nowak <kamil@kamils-air.home>
2022-12-15 10:27:10 +0100

    add new information about variables

--- src/vars.rs
+++ src/vars.rs
@@ -13,4 +13,14 @@ pub fn run() {
     // Assign multiple vars
     let (my_name, my_age) = ("Kamil", 24);
     println!("My name is {} and I am {}", my_name, my_age);
+
+    const COUNT: u32 = 100_000; //cannot be mutable, we cannot return it and we have to set the type of const.
+    // default i32
+    let a = 92; // Decimal
+    let b = 0xff; // Hex
+    let c = 0o77; // Octal
+    let d = 0b1111_00; // Binary
+    let e = b'A'; // Byte (u8 only)
+
+
 }
