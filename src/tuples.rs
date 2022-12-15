abe5f0ca9bf9d42805330556e76139980748aa93
Kamil Nowak <kamil@kamils-air.home>
2022-12-15 10:31:32 +0100

    add new information about tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -6,4 +6,8 @@ pub fn run() {
     println!("{} is from {} and is {}", person.0, person.1, person.2);
 
     println!("{:?}", person);
+
+    let (first_name, last_name, age) = person;
+
+    println!("{} is from {} and is {}", first_name, last_name, age);
 }
