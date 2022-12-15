4cef3986e84fdf1d2c62348be48e2ecd732845d0
Kamil Nowak <kamil@kamils-air.home>
2022-12-15 10:19:43 +0100

    add color

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::*;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
@@ -13,23 +14,25 @@ pub fn run() {
     let mut is_winning = false;
 
     while !is_winning {
-        let mut guess = String::from("");
+        let mut guess = String::new();
         println!("Please input our guess");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed");
 
-        let guess: u32 = guess
+        let guess: u32 = match guess
             .trim()
-            .parse()
-            .expect("Failed(Wrong datatype)");
+            .parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
         match guess.cmp(&secret_number) {
-            Ordering:: Less => println!("Too small!"),
-            Ordering:: Greater => println!("Too big"),
+            Ordering:: Less => println!("{}", "Too small!".red()),
+            Ordering:: Greater => println!("{}", "Too big!".red()),
             Ordering:: Equal => {
                 is_winning = true;
-                println!("You win!");
+                println!("{}", "You win!".yellow());
             }
         }
     }
